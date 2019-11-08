da1fec2b03ddeb55d45b7c15cb3e61af2958d238
GD <gautier.dulac@polytechnique.Edu>
2019-11-08 16:21:52 +0100

    UPDATE - Lower nb of epochs

--- modelling.py
+++ modelling.py
@@ -14,7 +14,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 fpath = 'data/imagenet_class_index.json'
 
 batch_size_preconvfeat = 128
-num_epochs = 50
+num_epochs = 20
 # Define a loss function
 criterion = nn.NLLLoss()
 # Define an optimizer function
