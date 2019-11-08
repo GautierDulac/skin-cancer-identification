615c7202acc611cb8c79cfbec74c0d29346e4960
leonardvinc <leonard.fleutot@polytechnique.edu>
2019-11-08 17:18:39 +0100

    testing for new models

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ import torch
 import torch.nn as nn
 
 ###Constants
-model_vgg = models.vgg16(pretrained=True)
+model_select = int(input("Select your model : Enter 1 for vgg , 2 for resnet 18, 3 for resnet 50"))
+if model_select == 1:
+    model_applied = models.vgg16(pretrained=True)
+elif model_select == 2:
+    model_applied = models.resnet18(pretrained=True)
+else:
+    model_applied = models.resnext50_32x4d(pretrained=True)
+
+
 batch_size_train=64
 batch_size_val=5
 batch_size_preconvfeat = 128
@@ -20,15 +28,15 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 ###Main function
 
-for param in model_vgg.parameters():
+for param in model_applied.parameters():
     param.requires_grad = False
-model_vgg.classifier._modules['6'] = nn.Linear(4096, 2)
-model_vgg.classifier._modules['7'] = torch.nn.LogSoftmax(dim = 1)
+model_applied.classifier._modules['6'] = nn.Linear(4096, 2)
+model_applied.classifier._modules['7'] = torch.nn.LogSoftmax(dim = 1)
 
-model_vgg = model_vgg.to(device)
+model_applied = model_applied.to(device)
 
 
-predictions, all_proba, all_classes = validation_model_preconvfeat(model_vgg, batch_size_train, batch_size_val, shuffle_train, shuffle_val, batch_size_preconvfeat, num_workers)
+predictions, all_proba, all_classes = validation_model_preconvfeat(model_applied, batch_size_train, batch_size_val, shuffle_train, shuffle_val, batch_size_preconvfeat, num_workers)
 
 print(predictions)
 
