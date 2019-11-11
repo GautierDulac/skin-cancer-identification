cfa7dcb6bdc29b0f2e72a651cb7594fcd12fa0e2
GD <gautier.dulac@polytechnique.Edu>
2019-11-11 14:13:39 +0100

    UPDATE - Debug criterion

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ else:
 
 model_applied = model_applied.to(device)
 predictions, all_proba, all_classes = validation_model_preconvfeat(model_applied, batch_size_train, batch_size_val,
-                                                                   shuffle_train, shuffle_val, num_workers, optimizer)
+                                                                   shuffle_train, shuffle_val, num_workers, optimizer, criterion)
 
 print(predictions)
 final_visualisation(predictions, all_classes, prepare_dsets())
