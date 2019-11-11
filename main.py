e6f1598d5eda90e4ff0b92671290fb5007eb2b36
leonardvinc <leonard.fleutot@polytechnique.edu>
2019-11-11 14:35:34 +0100

    debug mobilenet

--- main.py
+++ main.py
@@ -30,15 +30,22 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 for param in model_applied.parameters():
     param.requires_grad = False
 
-if model_select == 2:
+
+if model_select == 1 :
+    model_applied.classifier._modules['6'] = nn.Linear(4096, 2)
+    model_applied.classifier._modules['7'] = torch.nn.LogSoftmax(dim=1)
+    optimizer = torch.optim.Adam(model_applied.classifier.parameters())
+    criterion = nn.NLLLoss()
+
+elif model_select == 2:
     num_ftrs = model_applied.fc.in_features
     model_applied.fc = nn.Linear(num_ftrs, 2)
     optimizer = optim.SGD(model_applied.fc.parameters(), lr=0.001, momentum=0.9)
     criterion = nn.CrossEntropyLoss()
 
 else:
-    model_applied.classifier._modules['6'] = nn.Linear(4096, 2)
-    model_applied.classifier._modules['7'] = torch.nn.LogSoftmax(dim=1)
+    model_applied.classifier._modules['1'] = nn.Linear(1280, 2)
+    model_applied.classifier._modules['2'] = torch.nn.LogSoftmax(dim=1)
     optimizer = torch.optim.Adam(model_applied.classifier.parameters())
     criterion = nn.NLLLoss()
 
