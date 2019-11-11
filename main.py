0a71ab24e28f45b21c63974f82c05e2cb972479c
GD <gautier.dulac@polytechnique.Edu>
2019-11-11 14:03:58 +0100

    UPDATE - Debug & reformat

--- main.py
+++ main.py
@@ -16,16 +16,14 @@ elif model_select == 2:
 else:
     model_applied = models.mobilenet(pretrained=True)
 
-
-batch_size_train=64
-batch_size_val=5
+batch_size_train = 64
+batch_size_val = 5
 batch_size_preconvfeat = 128
-shuffle_train=True
-shuffle_val=False
+shuffle_train = True
+shuffle_val = False
 
-num_workers=6
+num_workers = 6
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 
 ###Main function
 
@@ -34,22 +32,21 @@ for param in model_applied.parameters():
 
 if model_select == 1:
     model_applied.classifier._modules['6'] = nn.Linear(4096, 2)
-    model_applied.classifier._modules['7'] = torch.nn.LogSoftmax(dim = 1)
+    model_applied.classifier._modules['7'] = torch.nn.LogSoftmax(dim=1)
     optimizer = torch.optim.Adam(model_applied.classifier.parameters())
     criterion = nn.NLLLoss()
 
 else:
     num_ftrs = model_applied.fc.in_features
     model_applied.fc = nn.Linear(num_ftrs, 2)
-    optimizer =  optim.SGD(model_applied.fc.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(model_applied.fc.parameters(), lr=0.001, momentum=0.9)
     criterion = nn.CrossEntropyLoss()
-
 
 model_applied = model_applied.to(device)
 predictions, all_proba, all_classes = validation_model_preconvfeat(model_applied, batch_size_train, batch_size_val,
-                                                                       shuffle_train, shuffle_val, num_workers, optimizer)
+                                                                   shuffle_train, shuffle_val, num_workers, optimizer)
 
 print(predictions)
 final_visualisation(predictions, all_classes, prepare_dsets())
-validation_model_preconvfeat(model, batch_size_train, batch_size_val, shuffle_train, shuffle_valid,
-                                 batch_size_preconvfeat, num_workers)
+# validation_model_preconvfeat(model, batch_size_train, batch_size_val, shuffle_train, shuffle_valid,
+# batch_size_preconvfeat, num_workers)
