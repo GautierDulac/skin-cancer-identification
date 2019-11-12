fac094980f849a9fb21a2b190b2eccc45855df5b
GD <gautier.dulac@polytechnique.Edu>
2019-11-12 09:26:53 +0100

    UPDATE - validity check in parameters

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ model_select = int(input("Select your model : Enter 1 for vgg , 2 for resnet 18,
 num_epochs = int(input("Select epochs number\n"))
 lr = float(input("Select learning rate\n"))
 
+if (not (model_select in [1, 2, 3])) or num_epochs <= 0 or lr <= 0:
+    raise ValueError("Paramètres incorrects")
+
 if model_select == 1:
     model_applied = models.vgg16(pretrained=True)
 elif model_select == 2:
@@ -33,8 +36,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 for param in model_applied.parameters():
     param.requires_grad = False
 
-
-if model_select == 1 :
+if model_select == 1:
     model_applied.classifier._modules['6'] = nn.Linear(4096, 2)
     model_applied.classifier._modules['7'] = torch.nn.LogSoftmax(dim=1)
     optimizer = torch.optim.Adam(model_applied.classifier.parameters(), lr=lr)
@@ -54,20 +56,19 @@ else:
 
 model_applied = model_applied.to(device)
 
-
 predictions, all_proba, all_classes = validation_model_preconvfeat(model_applied, batch_size_train, batch_size_val,
-                                                                   shuffle_train, shuffle_val, num_workers, optimizer, criterion, model_select, num_epochs, lr)
+                                                                   shuffle_train, shuffle_val, num_workers, optimizer,
+                                                                   criterion, model_select, num_epochs, lr)
 
 print(predictions)
 final_visualisation(predictions, all_classes, prepare_dsets())
 
 if model_select == 2:
-    file_name = str(input('Please Select the file on which to save the activation map : Enter the path from the project directory\n'))
+    file_name = str(input(
+        'Please Select the file on which to save the activation map : Enter the path from the project directory\n'))
     save_name = str(input('Save the image to the following name\n'))
 
     activation_map(model_applied, file_name, save_name)
 
 # validation_model_preconvfeat(model, batch_size_train, batch_size_val, shuffle_train, shuffle_valid,
 # batch_size_preconvfeat, num_workers)
-
-
